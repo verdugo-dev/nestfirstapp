b5a8c43e544d6dea4325957f0f33206fcbfcab6f
Carlos Verdugo <carlos@adroitoverseas.com>
2026-03-28 14:48:01 -0700

    Add task controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+
+    @Get()
+    helloworld() {
+        return "Hello world";
+    }
+
+}
