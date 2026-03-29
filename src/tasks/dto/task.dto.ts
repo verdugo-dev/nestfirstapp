e6925815eefb3497b81c6896b0534bc33ac5627e
Carlos Verdugo <carlos@adroitoverseas.com>
2026-03-28 20:16:00 -0700

    Adding validators

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,27 @@
 import { TaskStatus } from "../task.entity"
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class CreateTaskDTO {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string
+
+    @IsString()
     description: string
 }
 
 export class UpdateTaskDTO {
+    @IsString()
+    @IsOptional()
     title?: string
+
+    @IsString()
+    @IsOptional()
     description?: string
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
     status?: TaskStatus;
 }
